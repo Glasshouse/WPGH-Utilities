41f27b3ae322ade99e7b6bd73b0358f9e212b775
Paul BERNARD <paul@glasshouse.fr>
2020-11-08 11:29:07 +0100

    Add plugin update from Github

--- wpgh-utilities.php
+++ wpgh-utilities.php
@@ -20,6 +20,8 @@
 
 define('TEMPLATE_URL', get_template_directory_uri());
 
+include_once(__DIR__ . '/updater/updater.php');
+
 add_action('init', 'wpgh_utilities_init');
 function wpgh_utilities_init()
 {
@@ -29,4 +31,21 @@ function wpgh_utilities_init()
     require_once __DIR__ . '/security.php';
 
     disable_emojis();
+
+    if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+        $config = array(
+            'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+            'proper_folder_name' => 'wpgh-utilities', // this is the name of the folder your plugin lives in
+            'api_url' => 'https://api.github.com/repos/Glasshouse/WPGH-Utilities', // the GitHub API url of your GitHub repo
+            'raw_url' => 'https://raw.github.com/Glasshouse/WPGH-Utilities/main', // the GitHub raw url of your GitHub repo
+            'github_url' => 'https://github.com/Glasshouse/WPGH-Utilities', // the GitHub url of your GitHub repo
+            'zip_url' => 'https://github.com/Glasshouse/WPGH-Utilities/zipball/main', // the zip url of the GitHub repo
+            'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+            'requires' => '5.0', // which version of WordPress does your plugin require?
+            'tested' => '5..0', // which version of WordPress is your plugin tested up to?
+            'readme' => 'README.md', // which file to use as the readme for the version number
+            'access_token' => '', // Access private repositories by authorizing under Plugins > GitHub Updates when this example plugin is installed
+        );
+        new WP_GitHub_Updater($config);
+    }
 }
